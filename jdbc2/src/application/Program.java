823247ea3530c62888cb2d17d2bc6ec976374ee7
Marco <marcoamorim95@hotmail.com>
2019-05-21 16:05:29 -0300

    Java and Database exercises

---
+++ jdbc2/src/application/Program.java
@@ -0,0 +1,39 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import db.DB;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+
+		try {
+			conn = DB.getConnection();
+
+			st = conn.createStatement();
+
+			rs = st.executeQuery("select * from seller");
+
+			while (rs.next()) {
+				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name") + ", " + rs.getString("Email") + ", "
+						+ rs.getDate("BirthDate") + ", " + rs.getDouble("BaseSalary") + ", "
+						+ rs.getInt("DepartmentId"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+	}
+
+}
